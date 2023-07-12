6474c7138a517d5e0669aacabd78fcef1eef93a9
ItsiNFY7 <133520445+ItsiNFY7@users.noreply.github.com>
2023-07-12 12:19:37 -0400

    Add files via upload

---
+++ fashionbrand.py
@@ -0,0 +1,4 @@
+print("Welcome to my fashion brand name Generator")
+color = input("What is your favorite color? \n")
+season = input("What is your favorite season? \n")
+print("Your fashion brand name could be " + color + " " + season)
